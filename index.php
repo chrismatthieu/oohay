7462240dd2a3955ac75ac7d99517ff9715e639fe
Chris Matthieu <chris@matthieu.us>
2018-08-16 10:59:35 -0700

    removed https redirect

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
-    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $redirect);
-    exit();
-}
 header( 'Location: /index.html' ) ;
 ?>
